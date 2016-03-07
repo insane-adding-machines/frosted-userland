363334de7ab3c95a657d2a4bc101096bc0a43809
brabo <brabo.sil@gmail.com>
2016-03-07 19:26:13 +0100

    wc: refactor & attempt to add stdin as input (fail)

--- binutils/wc.c
+++ binutils/wc.c
@@ -1,88 +1,128 @@
 #include "frosted_binutils.h"
 
+#define LINES		1
+#define WORDS		2
+#define BYTES		4
 
-int parse_opts(int argc, char *args[], int *flags) {
+#define IN		0
+#define OUT		1
+
+struct wc {
+	long lines;
+	long words;
+	long bytes;
+};
+
+/* Parse our arguments */
+int parse_opts(int argc, char *args[], int *flags)
+{
 	int count = 0;
 	int c;
 
 	if( argc < 2 ){
-		fprintf(stderr, "Usage: wc [OPTION]... [FILE]\n");
+		fprintf(stderr, "Usage: wc [OPTION]... [FILE]\r\n");
 		exit(1);
 	}
 
 	*flags = 0;
 
-	while ((c = getopt(argc, args, "lcw")) != -1) {
+	while ((c = getopt(argc, args, "lwc")) != -1) {
 		switch (c) {
-			case 'c':
-				*flags += 0x01;
+			case 'l':
+				*flags += LINES;
 				count++;
 				break;
 			case 'w':
-				*flags += 0x02;
+				*flags += WORDS;
 				count++;
 				break;
-			case 'l':
-				*flags += 0x04;
+			case 'c':
+				*flags += BYTES;
 				count++;
 				break;
 			default:
-				fprintf(stderr,"wc: invalid option -'%c'\r\n", c);
-				exit(1);
+				//fprintf(stderr, "wc: invalid option -'%c'\r\n", c);
+				break;
 			}
 	}
 	return count;
 }
 
+/* Word Count a file */
+int wc_count(int fd, struct wc *wc)
+{
+	int r;
+	char c;
+	int state = OUT;
 
+	while ((r = read(fd, &c, 1)) > 0) {
+		if ((c == EOF)) {
+			break;
+		}
+		wc->bytes++;
+		if (c == '\n') {
+			wc->lines++;
+		}
+		if ((c == ' ') || (c == '\t') || (c == '\n')) {
+			state = OUT;
+		} else if (state == OUT) {
+			wc->words++;
+			state = IN;
+		}
+	}
+
+	if (state == IN) {
+		wc->lines++;
+	}
+
+	return 0;
+}
+
+/* Print our selected wc outputs */
+int wc_print(char *file, struct wc *wc, int flags)
+{
+	if ((flags & LINES) || !flags) {
+		printf("%ld ", wc->lines);
+	} if ((flags & WORDS)  || !flags) {
+		printf("%ld ", wc->words);
+	} if ((flags &  BYTES) || !flags) {
+		printf("%ld ", wc->bytes);
+	}
+	printf(" %s\r\n", file);
+
+	return 0;
+}
+
+/* main: parse args, loop over FILES, count and print results */
 int main(int argc, char *args[])
 {
-	int fd;
 	int i = 1;
 	int flags;
+	int fd;
+
 	i += parse_opts(argc, args, &flags);
 
 	while (args[i]) {
-		int last_white = 0;
-		int bytes = 0, words = 0, newlines = 0;
-		fd = open(args[i], O_RDONLY);
-		if (fd < 0) {
-			printf("File not found.\r\n");
-			exit(-1);
+		struct wc wc = {0, 0, 0};
+		if (!strncmp(args[i], "-", 1)) {
+			printf("trying stdin...\r\n");
+			fd = STDIN_FILENO;
 		} else {
-			int r;
-			char buf[1];
-			do {
-				r = read(fd, buf, 1);
-				if (r > 0) {
-					bytes += r;
-					if (buf[0] == '\n') {
-						newlines++;
-						words++;
-						last_white = 1;
-					} else if ((buf[0] == ' ') || (buf[0] == '\t')) {
-						words++;
-						last_white = 1;
-					} else {
-						last_white = 0;
-					}
-				}
-			} while (r > 0);
-			close(fd);
+			fd = open(args[i], O_RDONLY);
 		}
-		if (!last_white) {
-			words++;
-			newlines++;
+
+		if (fd < 0) {
+			fprintf(stderr, "File %s not found.\r\n", args[i]);
+			i++;
+			continue;
 		}
-		if ((flags & 0x04) || !flags) {
-			printf("%4d ", newlines);
-		} if ((flags & 0x02)  || !flags) {
-			printf("%4d ", words);
-		} if ((flags &  0x01) || !flags) {
-			printf("%4d ", bytes);
+
+		if (!wc_count(fd, &wc)) {
+			wc_print(args[i], &wc, flags);
 		}
-		printf(" %s\r\n", args[i]);
+		close(fd);
 		i++;
 	}
+
 	exit(0);
 }
