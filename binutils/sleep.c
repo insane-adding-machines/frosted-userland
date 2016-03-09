bd3d335119a8dc938d9470a0152e1cec609ce075
brabo <brabo.sil@gmail.com>
2016-03-09 03:53:47 +0100

    binutils: added yes and sleep (sleep non functional atm)

---
+++ binutils/sleep.c
@@ -0,0 +1,35 @@
+/*
+ *      This file is part of frosted.
+ *
+ *      frosted is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License version 2, as
+ *      published by the Free Software Foundation.
+ *
+ *
+ *      frosted is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with frosted.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Authors: brabo
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char *args[])
+{
+	if ((argc < 2) || (argc > 2)) {
+		fprintf(stderr, "Usage: sleep TIME\r\n");
+	}
+	long sec = strtol(args[1], NULL, 10);
+	sleep(sec);
+	exit(0);
+}
---
+++ binutils/yes.c
@@ -0,0 +1,43 @@
+/*
+ *      This file is part of frosted.
+ *
+ *      frosted is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License version 2, as
+ *      published by the Free Software Foundation.
+ *
+ *
+ *      frosted is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with frosted.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Authors: brabo
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char *args[])
+{
+	while (1 < 2) {
+		if (argc > 1) {
+			int i = 1;
+			while (args[i]) {
+				printf("%s ", args[i]);
+				i++;
+			}
+			printf("\r\n");
+		} else {
+			printf("y\r\n");
+		}
+	}
+	exit(0);
+}
