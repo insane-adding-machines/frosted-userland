e4e5f331150e4d38895082d0957c272e68d2d1c4
Daniele Lacamera <root@danielinux.net>
2016-07-03 18:56:06 +0200

    Added reboot binary

---
+++ binutils/reboot.c
@@ -0,0 +1,33 @@
+/*
+ *      This file is part of frosted.
+ *
+ *      frosted is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License version 2, as
+ *      published by the Free Software Foundation.
+ *
+ *
+ *      frosted is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with frosted.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Authors: danielinux
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/reboot.h>
+
+int main(int argc, char *args[])
+{
+
+    fprintf(stderr, "Rebooting frosted NOW!\r\n");
+    fflush(stderr);
+    usleep(500000);
+    reboot();
+    exit(0); /* Never reached */
+}
