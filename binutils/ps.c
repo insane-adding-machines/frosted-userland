e6dcd6c9a1454b9daaf75d542aa1974b2f85d566
Antonio Cardace <anto.cardace@gmail.com>
2017-05-28 16:16:25 +0200

    add 'ps' command

---
+++ binutils/ps.c
@@ -0,0 +1,44 @@
+/*
+ *      This file is part of frosted.
+ *
+ *      frosted is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License version 2, as
+ *      published by the Free Software Foundation.
+ *
+ *
+ *      frosted is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with frosted.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Authors: Antonio Cardace
+ *
+ */
+
+#include "frosted_binutils.h"
+
+#define TASKS_FILE "/sys/tasks"
+
+int main(int argc, char *args[])
+{
+    int fd;
+    fd = open(TASKS_FILE, O_RDONLY);
+    if (fd < 0) {
+        printf("File not found.\r\n");
+        exit(5);
+    } else {
+        int r;
+        char buf[64];
+        do {
+            r = read(fd, buf, 64);
+            if (r > 0) {
+                write(STDOUT_FILENO, buf, r);
+            }
+        } while (r > 0);
+        close(fd);
+    }
+    exit(0);
+}
