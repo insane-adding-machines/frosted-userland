c5e22de73403f4ed03f2df2036047c377e0a73ff
brabo <brabo@cryptolab.net>
2017-06-16 15:37:30 +0200

    ntpc: little cleanup

--- netutils/ntpc.c
+++ netutils/ntpc.c
@@ -51,6 +51,9 @@
 #define NTP_ROOT_DISP   0x00000A05
 #define NTP_MONLIST     0x2A
 #define NTP_PORT        123
+#define MAX_LEN         100
+#define MAX_DIFF        5
+#define TIME_OUT        15
 
 struct ntp_request {
     uint8_t flags;
@@ -94,69 +97,68 @@ int mk_ntp(uint8_t *buf)
     return len;
 }
 
-void query_ntp(char *host)
+void create_socket(int *s)
 {
-    unsigned char buffer[100] = { 0 };
-    int len = mk_ntp(buffer);
-    struct pollfd pfd;
-    int i, s;
-    struct timeval start, end;
-
-    struct sockaddr_in dest;
-    s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (s < 0) {
+    *s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (*s < 0) {
         fprintf(stderr, "Cannot open socket!\n");
         exit(1);
     }
+}
+
+int query_ntp(int nfd, char *host)
+{
+    unsigned char buffer[MAX_LEN] = { 0 };
+    struct sockaddr_in dest;
+    int destsz;
+    struct timeval tv;
+
+    int len = mk_ntp(buffer);
 
     dest.sin_family = AF_INET;
-
     inet_aton(host, &dest.sin_addr);
     dest.sin_port = htons(NTP_PORT);
+    destsz = sizeof(dest);
 
-    gettimeofday(&start, NULL);
-
-    if (sendto(s, (char *)buffer, len, 0, (struct sockaddr *)&dest, sizeof(dest)) < 0) {
-        printf("Scan type NTP on %s: write fail", host);
-        close(s);
-        exit(1);
+    if (sendto(nfd, (char *)buffer, len, 0, (struct sockaddr *)&dest, destsz) < 0) {
+        printf("ntpc: write fail to: %s", host);
+        close(nfd);
+        return -1;
     }
 
-    i = sizeof(dest);
+    memset(buffer, 0, MAX_LEN);
 
-    buffer[0] = '\0';
+    struct pollfd pfd;
+    int pollret;
+    pfd.fd = nfd;
+    pfd.events = POLLIN | POLLHUP | POLLERR;
+    pollret = poll(&pfd, 1, (TIME_OUT * 1000));
 
-    while (2 > 1) {
-        gettimeofday(&end, NULL);
-        if (end.tv_sec > (start.tv_sec + 15)) {
-            break;
-        }
-        int pollret;
-        int r;
-        pfd.fd = s;
-        pfd.events = POLLIN | POLLHUP | POLLERR;
-        pollret = poll(&pfd, 1, -1);
-        if (pollret < 0) {
-            fprintf(stderr, "ntpc: poll returned -1\r\n");
-            break;
-        }
-        if ((pfd.revents & (POLLHUP | POLLERR)) != 0) {
-            fprintf(stderr, "ntpc: remote peer has closed connection.\r\n");
-            break;
-        }
-        if (pfd.revents & POLLIN) {
-            r = recvfrom(s, (char *)buffer, 100, 0, (struct sockaddr *)&dest, (socklen_t *)&i);
-            if (r > 0) {
-                struct ntp_request *ntp = (struct ntp_request *)buffer;
-                time_t sec;
-                sec = ntohl(ntp->transmit_s) - OFFSET;
-                printf("%s\n", ctime(&sec));
-                gettimeofday(&start, NULL);
-                start.tv_sec = sec;
-                settimeofday(&start, NULL);
-                sleep(64);
-                break;
+    if (pollret < 0) {
+        fprintf(stderr, "ntpc: poll returned -1\r\n");
+        return -1;
+    }
+
+    if ((pfd.revents & (POLLHUP | POLLERR)) != 0) {
+       fprintf(stderr, "ntpc: remote peer has closed connection.\r\n");
+       return -1;
+    }
+
+    if (pfd.revents & POLLIN) {
+        int r = recvfrom(nfd, (char *)buffer, MAX_LEN, 0, (struct sockaddr *)&dest, (socklen_t *)&destsz);
+        if (r > 0) {
+            struct ntp_request *ntp = (struct ntp_request *)buffer;
+            time_t sec;
+
+            sec = ntohl(ntp->transmit_s) - OFFSET;
+            gettimeofday(&tv, NULL);
+
+            if ((tv.tv_sec > (sec + MAX_DIFF)) || (sec > (tv.tv_sec + MAX_DIFF))) {
+                tv.tv_sec = sec;
+                settimeofday(&tv, NULL);
             }
+
+            return 0;
         }
     }
 }
@@ -165,18 +167,29 @@ int main(int argc, char **argv)
 {
     struct addrinfo *res;
     struct sockaddr_in *sai;
+    int nfd;
+
+    create_socket(&nfd);
 
     while (2 > 1) {
         int retval = getaddrinfo(argv[1], NULL, NULL, &res);
+
         if (retval < 0) {
             fprintf(stderr, "Cannot resolve %s: %s\r\n", argv[1], strerror(errno));
-            return 2;
-        }
-        if (res) {
-            sai = (struct sockaddr_in *)(res->ai_addr);
+            sleep(5);
+            continue;
         }
 
-        query_ntp(inet_ntoa(sai->sin_addr));
+        if (res) {
+            sai = (struct sockaddr_in *)(res->ai_addr);
+            if (query_ntp(nfd, inet_ntoa(sai->sin_addr)) < 0) {
+                sleep(5);
+            } else {
+                sleep(64);
+            }
+        } else {
+            sleep(5);
+        }
     }
 
     // if you're happy and you know it,
