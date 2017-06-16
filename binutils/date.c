0ab8d7d9dda926d10692109416a8ca37e7ea7f91
brabo <brabo@cryptolab.net>
2017-06-16 02:10:25 +0200

    binutils: added date

---
+++ binutils/date.c
@@ -0,0 +1,34 @@
+/*
+ *      This file is part of frosted.
+ *
+ *      frosted is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License version 2, as
+ *      published by the Free Software Foundation.
+ *
+ *
+ *      frosted is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with frosted.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Authors: brabo
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+
+int main(int argc, char **argv)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    printf("%s\n", ctime(&tv.tv_sec));
+
+    // if you're happy and you know it,
+    exit(0);
+}
