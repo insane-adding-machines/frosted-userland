06972f5ef1b8023fae0ce7938f2568e90c30b1ef
Maxime Vincent <maxime.vince@gmail.com>
2017-10-25 21:40:39 +0200

    Working veneer for bFLT shared libs

--- shared-lib/demo_lib/lib.c
+++ shared-lib/demo_lib/lib.c
@@ -3,10 +3,14 @@
 
 #include <stdio.h>
 
-int main() {
-    printf("Library id %d initialized\n", LIB_ID);
-    return 0;
+void _start(void) {
+    // stub
 }
+
+//int main() {
+//    printf("Library id %d initialized\n", LIB_ID);
+//    return 0;
+//}
 
 int library_call() {
     printf("Library id %d called!\n", LIB_ID);
